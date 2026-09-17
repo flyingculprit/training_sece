d0beabdb7b8d8b735ad2e922f5c61039dccd229e
Surya <110118334+flyingculprit@users.noreply.github.com>
2026-09-17 12:46:31 +0530

    pkg

--- demo2/src/main/java/com/example/demo/controller/AccountController.java
+++ demo2/src/main/java/com/example/demo/controller/AccountController.java
@@ -1,7 +1,7 @@
 package com.example.bank.controller;
 
-import com.example.demo.model.Account;
-import com.example.demo.service.AccountService;
+import com.example.bank.model.Account;
+import com.example.bank.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
